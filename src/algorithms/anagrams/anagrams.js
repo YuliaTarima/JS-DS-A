8cd37dd2d77368e52a5d45670c16318b6c85eb61
Yulia Tarima <yulia.tarima@gmail.com>
2020-08-17 15:56:00 -0700

    Anagrams

--- src/algorithms/anagrams/anagrams.js
+++ src/algorithms/anagrams/anagrams.js
@@ -1,12 +1,67 @@
-import {isEqual} from 'lodash';
-import {getLetterCount} from './letter-count';
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
 
-export const formatString = string =>
-    string.toLowerCase().replace(/\s+/g, '');
+// // Solution
+// import {isEqual} from 'lodash';
+// import {getLetterCount} from './letter-count';
+//
+// export const formatString = string =>
+//     string.toLowerCase().replace(/\s+/g, '');
+//
+// function anagrams(stringA, stringB) {
+//     const string1LetterCount = getLetterCount(formatString(stringA));
+//     const string2LetterCount = getLetterCount(formatString(stringB));
+//
+//     return isEqual(string1LetterCount, string2LetterCount);
+// };
 
-export const isAnagram = (string1, string2) => {
-    const string1LetterCount = getLetterCount(formatString(string1));
-    const string2LetterCount = getLetterCount(formatString(string2));
+// // Solution
+// function cleanString(str) {
+//     return str
+//         .replace(/[^\w]/g, '')
+//         .toLowerCase()
+//         .split('')
+//         .sort()
+//         .join('');
+// }
+//
+// function anagrams(stringA, stringB) {
+//     return cleanString(stringA) === cleanString(stringB);
+// }
 
-    return isEqual(string1LetterCount, string2LetterCount);
-};
+// Solution
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+}
+
+module.exports = anagrams;
--- src/algorithms/anagrams/anagrams.test.js
+++ src/algorithms/anagrams/anagrams.test.js
@@ -1,52 +1,80 @@
-import {expect} from 'chai';
-import {formatString, isAnagram} from './anagrams';
+const anagrams = require('./anagrams');
 
-describe('formatString - basic functionality', () => {
-    it('removes spaces and converts to lower case', () => {
-        // 'This is a STRING' -> 'thisisastring'
-        const expected = 'thisisastring';
-        const actual = formatString('This is a STRING');
-        expect(actual).to.equal(expected);
-    });
+test('anagrams function exists', () => {
+    expect(typeof anagrams).toEqual('function');
 });
 
-describe('isAnagram - basic functionality', () => {
-// 'listen' 'silent'
-// 'elbow' 'below'
-    it('returns true when two known anagrams are passed in', () => {
-        const expected = true;
-        const actual = isAnagram('listen', 'silent');
-        expect(actual).to.equal(expected);
-    });
+test('"hello" is an anagram of "llohe"', () => {
+    expect(anagrams('hello', 'llohe')).toBeTruthy();
+});
 
-// 'elbows' 'below' NOT anagrams
-    it('returns false when either of the strings has extra letters', () => {
-        const expected = false;
-        const actual = isAnagram('elbows', 'below');
-        expect(actual).to.equal(expected);
+test('"Whoa! Hi!" is an anagram of "Hi! Whoa!"', () => {
+    expect(anagrams('Whoa! Hi!', 'Hi! Whoa!')).toBeTruthy();
+});
 
-        const actual2 = isAnagram('below', 'elbows');
-        expect(actual2).to.equal(expected);
-    })
+test('"One One" is not an anagram of "Two two two"', () => {
+    expect(anagrams('One One', 'Two two two')).toBeFalsy();
+});
 
-// 'listens' 'silent' NOT anagrams
-    it('returns false when the strings have the same letters in different quantities', () => {
-        const expected = false;
-        const actual = isAnagram('listens', 'silent');
-        expect(actual).to.equal(expected);
-    });
+test('"One one" is not an anagram of "One one c"', () => {
+    expect(anagrams('One one', 'One one c')).toBeFalsy();
+});
 
-// 'conversation' 'voices rant on' ARE anagrams
-    it('ignores spaces in the input strings', () => {
-        const expected = true;
-        const actual = isAnagram('conversation', 'voices rant on');
-        expect(actual).to.equal(expected);
-    });
+test('"A tree, a life, a bench" is not an anagram of "A tree, a fence, a yard"', () => {
+    expect(
+        anagrams('A tree, a life, a bench', 'A tree, a fence, a yard')
+    ).toBeFalsy();
+});
 
-// 'STATE' 'taste' ARE anagrams
-    it('ignores case in the input strings', () => {
-        const expected = true;
-        const actual = isAnagram('STATE', 'taste');
-        expect(actual).to.equal(expected);
-    });
-})
+// import {expect} from 'chai';
+// import {formatString, isAnagram} from './anagrams';
+//
+// describe('formatString - basic functionality', () => {
+//     it('removes spaces and converts to lower case', () => {
+//         // 'This is a STRING' -> 'thisisastring'
+//         const expected = 'thisisastring';
+//         const actual = formatString('This is a STRING');
+//         expect(actual).to.equal(expected);
+//     });
+// });
+//
+// describe('isAnagram - basic functionality', () => {
+// // 'listen' 'silent'
+// // 'elbow' 'below'
+//     it('returns true when two known anagrams are passed in', () => {
+//         const expected = true;
+//         const actual = isAnagram('listen', 'silent');
+//         expect(actual).to.equal(expected);
+//     });
+//
+// // 'elbows' 'below' NOT anagrams
+//     it('returns false when either of the strings has extra letters', () => {
+//         const expected = false;
+//         const actual = isAnagram('elbows', 'below');
+//         expect(actual).to.equal(expected);
+//
+//         const actual2 = isAnagram('below', 'elbows');
+//         expect(actual2).to.equal(expected);
+//     })
+//
+// // 'listens' 'silent' NOT anagrams
+//     it('returns false when the strings have the same letters in different quantities', () => {
+//         const expected = false;
+//         const actual = isAnagram('listens', 'silent');
+//         expect(actual).to.equal(expected);
+//     });
+//
+// // 'conversation' 'voices rant on' ARE anagrams
+//     it('ignores spaces in the input strings', () => {
+//         const expected = true;
+//         const actual = isAnagram('conversation', 'voices rant on');
+//         expect(actual).to.equal(expected);
+//     });
+//
+// // 'STATE' 'taste' ARE anagrams
+//     it('ignores case in the input strings', () => {
+//         const expected = true;
+//         const actual = isAnagram('STATE', 'taste');
+//         expect(actual).to.equal(expected);
+//     });
+// })
